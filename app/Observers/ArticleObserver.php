361f0ca8679a3268ee823ec79d6a25c4b29768ae
aryalaashish121 <aryalaashish121@gmai.com>
2021-12-11 22:43:55 +0545

    cache destroy if article created/updated

---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Support\Facades\Cache;
+
+class ArticleObserver
+{
+    /**
+     * Handle the Article "created" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+        //destroy cache if new article created
+        $category = Category::find($article->category_id);
+        $this->destroyCache($category['slug']);
+    }
+
+    /**
+     * Handle the Article "updated" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function updated(Article $article)
+    {
+        $category = Category::find($article->category_id);
+        $this->destroyCache($category['slug']);
+    }
+
+    /**
+     * Handle the Article "deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function deleted(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "restored" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function restored(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Handle the Article "force deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function forceDeleted(Article $article)
+    {
+        //
+    }
+    private function destroyCache($key){
+        Cache::forget($key);
+    }
+}
